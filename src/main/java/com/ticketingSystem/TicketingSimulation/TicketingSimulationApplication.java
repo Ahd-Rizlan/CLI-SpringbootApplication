c8174299fa9e30cdb10af50d1fed2c9507882b4c
Ahamed Rizlan <arizlan@mitrai.com>
2024-11-23 22:45:42 +0530

    1st commit

---
+++ src/main/java/com/ticketingSystem/TicketingSimulation/TicketingSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.ticketingSystem.TicketingSimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSimulationApplication.class, args);
+	}
+
+}
