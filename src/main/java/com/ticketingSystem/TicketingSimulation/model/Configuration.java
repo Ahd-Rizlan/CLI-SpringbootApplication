38fbc643e4ce6eb7219c668ec77049f4cd094e57
Ahamed Rizlan <arizlan@mitrai.com>
2024-11-27 22:42:28 +0530

    Error Handling in Progerss

--- src/main/java/com/ticketingSystem/TicketingSimulation/model/Configuration.java
+++ src/main/java/com/ticketingSystem/TicketingSimulation/model/Configuration.java
@@ -1,14 +1,22 @@
 package com.ticketingSystem.TicketingSimulation.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@Table(name = "configuration_tbl")
+@Entity
 public class Configuration implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    private long id = 1L;
     private int totalTickets;//Number of tickets released at once
     private int maxTicketCapacity;//max a pool can hold at a moment
     private int ticketReleaseRate;
@@ -50,4 +58,11 @@ public class Configuration implements Serializable {
         this.customerRetrievalRate = customerRetrievalRate;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
---
+++ src/main/java/com/ticketingSystem/TicketingSimulation/repository/ConfigurationRepository.java
@@ -0,0 +1,11 @@
+package com.ticketingSystem.TicketingSimulation.repository;
+
+import com.ticketingSystem.TicketingSimulation.model.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ConfigurationRepository extends JpaRepository<Configuration, Long> {
+
+
+}
